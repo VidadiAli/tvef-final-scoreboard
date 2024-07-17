5ea20a14f6a67647453107afcff13b5d2a089385
VidadiAli <vidadiali.tiva@gmail.com>
2024-07-17 14:24:30 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,591 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import './App.css'
+import bcgVideo from './videos/bcg-loop.mp4'
+
+function App() {
+
+  const [scoreBoard, setScoreBoard] = useState([]);
+  const [juri, setJuri] = useState([]);
+  const [countOfJuri, setCountOfJuri] = useState(0);
+  const [nameOfJuri, setNameOfJuri] = useState('Jury');
+  const [juriCount, setJuriCount] = useState(0);
+  const [moveCount, setMoveCount] = useState(-2);
+  const [givenPuan, setGivenPuan] = useState([]);
+  const [getingCountry, setGetingCountry] = useState([]);
+  const [teleCount, setTeleCount] = useState(0);
+  const [teleCountConst, setTeleCountConst] = useState(0);
+
+  const [video, setVideo] = useState('');
+  const [bcgVideoLoop, setBcgVideoLoop] = useState('');
+
+  const [mainBoxArrayTele, setMainBoxArrayTele] = useState([]);
+  const [classOfBack, setClassOfBack] = useState('')
+
+  const [allCountries, setAllCountries] = useState([]);
+  const [finalists, setFinalists] = useState([]);
+
+
+  const colorOfName = [
+    {
+      id: 'and',
+      video: 'src/videos/and.mp4',
+      color1: "blue",
+      color2: "yellow",
+      color3: "red"
+    },
+    {
+      id: 'aus',
+      video: 'src/videos/aus.mp4',
+      color1: "blue",
+      color2: "white",
+      color3: "red"
+    },
+    {
+      id: 'avs',
+      video: 'src/videos/avs.mp4',
+      color1: "red",
+      color2: "white",
+      color3: "red"
+    },
+    {
+      id: 'aze',
+      video: 'src/videos/aze.mp4',
+      color1: "blue",
+      color2: "green",
+      color3: "red"
+    },
+    {
+      id: 'bel',
+      video: 'src/videos/bel.mp4',
+      color1: "red",
+      color2: "black",
+      color3: "orange"
+    },
+    {
+      id: 'bul',
+      video: 'src/videos/bul.mp4',
+      color1: "white",
+      color2: "green",
+      color3: "red"
+    },
+    {
+      id: 'bur',
+      video: 'src/videos/bur.mp4',
+      color1: "green",
+      color2: "red",
+      color3: "green"
+    },
+    {
+      id: 'can',
+      video: 'src/videos/can.mp4',
+      color1: "white",
+      color2: "red",
+      color3: "white"
+    },
+    {
+      id: 'cro',
+      video: 'src/videos/cro.mp4',
+      color1: "red",
+      color2: "white",
+      color3: "blue"
+    },
+    {
+      id: 'cyp',
+      video: 'src/videos/cyp.mp4',
+      color1: "white",
+      color2: "yellow",
+      color3: "white"
+    },
+    {
+      id: 'den',
+      video: 'src/videos/den.mp4',
+      color1: "red",
+      color2: "white",
+      color3: "red"
+    },
+    {
+      id: 'fin',
+      video: 'src/videos/fin.mp4',
+      color1: "white",
+      color2: "blue",
+      color3: "white"
+    },
+    {
+      id: 'fra',
+      video: 'src/videos/fra.mp4',
+      color1: "white",
+      color2: "blue",
+      color3: "red"
+    },
+    {
+      id: 'geo',
+      video: 'src/videos/geo.mp4',
+      color1: "white",
+      color2: "red",
+      color3: "white"
+    },
+    {
+      id: 'ger',
+      video: 'src/videos/ger.mp4',
+      color1: "black",
+      color2: "red",
+      color3: "orange"
+    },
+    {
+      id: 'ind',
+      video: 'src/videos/ind.mp4',
+      color1: "orange",
+      color2: "white",
+      color3: "green"
+    },
+    {
+      id: 'lux',
+      video: 'src/videos/lux.mp4',
+      color1: "red",
+      color2: "white",
+      color3: "deepskyblue"
+    },
+    {
+      id: 'mon',
+      video: 'src/videos/mon.mp4',
+      color1: "white",
+      color2: "red",
+      color3: "white"
+    },
+    {
+      id: 'mor',
+      video: 'src/videos/mor.mp4',
+      color1: "red",
+      color2: "green",
+      color3: "red"
+    },
+    {
+      id: 'net',
+      video: 'src/videos/net.mp4',
+      color1: "red",
+      color2: "white",
+      color3: "blue"
+    },
+    {
+      id: 'cal',
+      video: 'src/videos/cal.mp4',
+      color1: "blue",
+      color2: "red",
+      color3: "green"
+    },
+    {
+      id: 'nor',
+      video: 'src/videos/nor.mp4',
+      color1: "red",
+      color2: "white",
+      color3: "red"
+    },
+    {
+      id: 'pal',
+      video: 'src/videos/pal.mp4',
+      color1: "black",
+      color2: "white",
+      color3: "green"
+    },
+    {
+      id: 'pol',
+      video: 'src/videos/pol.mp4',
+      color1: "red",
+      color2: "white",
+      color3: "red"
+    },
+    {
+      id: 'rom',
+      video: 'src/videos/rom.mp4',
+      color1: "blue",
+      color2: "yellow",
+      color3: "red"
+    },
+    {
+      id: 'ser',
+      video: 'src/videos/ser.mp4',
+      color1: "red",
+      color2: "blue",
+      color3: "white"
+    },
+    {
+      id: 'kor',
+      video: 'src/videos/kor.mp4',
+      color1: "white",
+      color2: "blue",
+      color3: "red"
+    },
+    {
+      id: 'spa',
+      video: 'src/videos/spa.mp4',
+      color1: "orange",
+      color2: "red",
+      color3: "orange"
+    },
+    {
+      id: 'swi',
+      video: 'src/videos/swi.mp4',
+      color1: "red",
+      color2: "white",
+      color3: "red"
+    },
+    {
+      id: 'uga',
+      video: 'src/videos/uga.mp4',
+      color1: "black",
+      color2: "yellow",
+      color3: "red"
+    },
+    {
+      id: 'ukr',
+      video: 'src/videos/ukr.mp4',
+      color1: "blue",
+      color2: "yellow",
+      color3: "blue"
+    },
+    {
+      id: 'uni',
+      video: 'src/videos/uni.mp4',
+      color1: "blue",
+      color2: "red",
+      color3: "white"
+    },
+    {
+      id: 'vir',
+      video: 'src/videos/vir.mp4',
+      color1: "white",
+      color2: "yellow",
+      color3: "white"
+    },
+    {
+      id: 'wor',
+      video: 'src/videos/wor.mp4',
+      color1: "deepskyblue",
+      color2: "white",
+      color3: "deepskyblue"
+    }
+  ]
+
+  const givinPuans = document.getElementsByClassName('givin');
+  const totalPuans = document.getElementsByClassName('total');
+  const mainBox = document.getElementsByClassName('main-box');
+
+
+
+  const callData = async () => {
+    const scoreArray = (await axios.get('https://us-central1-api-tvef-vote.cloudfunctions.net/app/edition9')).data;
+    setScoreBoard(scoreArray.final);
+
+    const allJuri = (await axios.get('https://us-central1-api-tvef-vote.cloudfunctions.net/app/readPuans')).data;
+    setJuri(allJuri);
+
+
+    const arrayOfParticipants = (await axios.get('https://us-central1-api-tvef-vote.cloudfunctions.net/app/edition9')).data;
+    setAllCountries(arrayOfParticipants.final);
+
+    const arrayOfFinalists = (await axios.get('https://us-central1-api-tvef-vote.cloudfunctions.net/app/readTelePuans')).data;
+    setFinalists(arrayOfFinalists);
+  };
+
+
+
+
+
+
+  const showVotes = () => {
+    juri.forEach((e) => {
+      if (e.givinCountry === juri[juriCount].givinCountry) {
+        givenPuan.push(e.puan)
+        setGivenPuan(givenPuan);
+        getingCountry.push(e.getingCountry)
+        setGetingCountry(getingCountry);
+      }
+    });
+
+
+    setClassOfBack('back-ZIndex');
+
+    const givenCountry1 = document.createElement('span');
+    givenCountry1.className = 'givin-1'
+    givenCountry1.textContent = juri[juriCount].givinCountry.slice(0, juri[juriCount].givinCountry.length / 2);
+
+    const givenCountry2 = document.createElement('span');
+    givenCountry2.className = 'givin-2'
+    givenCountry2.textContent = juri[juriCount].givinCountry.slice(juri[juriCount].givinCountry.length / 2, juri[juriCount].givinCountry.length);
+
+    document.getElementsByClassName('back')[0].append(givenCountry1, givenCountry2);
+
+    setTimeout(() => {
+      givenCountry1.style.top = "50%";
+      givenCountry1.style.transform = "translateY(-50%)";
+
+      givenCountry2.style.bottom = "50%";
+      givenCountry2.style.transform = "translateY(50%)";
+    }, 700)
+
+    setTimeout(() => {
+      setClassOfBack('')
+      givenCountry1.remove();
+      givenCountry2.remove();
+    }, 3300)
+
+
+
+    givenCountry1.style.backgroundImage = `linear-gradient(45deg, ${colorOfName[Math.floor(moveCount / 2)].color1}, ${colorOfName[Math.floor(moveCount / 2)].color2}, ${colorOfName[Math.floor(moveCount / 2)].color3}, ${colorOfName[Math.floor(moveCount / 2)].color1}, ${colorOfName[Math.floor(moveCount / 2)].color2}, ${colorOfName[Math.floor(moveCount / 2)].color3}, ${colorOfName[Math.floor(moveCount / 2)].color1}, ${colorOfName[Math.floor(moveCount / 2)].color2}, ${colorOfName[Math.floor(moveCount / 2)].color3})`;
+
+    givenCountry2.style.backgroundImage = `linear-gradient(45deg, ${colorOfName[Math.floor(moveCount / 2)].color1}, ${colorOfName[Math.floor(moveCount / 2)].color2}, ${colorOfName[Math.floor(moveCount / 2)].color3}, ${colorOfName[Math.floor(moveCount / 2)].color1}, ${colorOfName[Math.floor(moveCount / 2)].color2}, ${colorOfName[Math.floor(moveCount / 2)].color3}, ${colorOfName[Math.floor(moveCount / 2)].color1}, ${colorOfName[Math.floor(moveCount / 2)].color2}, ${colorOfName[Math.floor(moveCount / 2)].color3})`;
+
+
+    setVideo("");
+
+  }
+
+
+
+  const addJuri = () => {
+    setVideo(colorOfName[Math.floor(moveCount / 2)].video);
+    console.log(juri)
+    setNameOfJuri(juri[juriCount].givinCountry);
+    for (let i = 0; i < givinPuans.length; i++) {
+      for (let j = 0; j < getingCountry.length; j++) {
+        if (givinPuans[i].previousElementSibling.previousElementSibling.textContent === getingCountry[j]) {
+          if (givenPuan[j] != 12) {
+            givinPuans[i].textContent = givenPuan[j];
+            totalPuans[i].textContent = (+totalPuans[i].textContent) + givenPuan[j];
+            document.getElementsByClassName('before-element')[i].style.width = "calc(100% - 125px)";
+          }
+        }
+      }
+      if (givinPuans[i].textContent != '0') {
+        givinPuans[i].style.display = 'inline-block';
+      }
+    }
+    setCountOfJuri(countOfJuri + 1);
+  }
+
+
+  const add12 = () => {
+    console.log(getingCountry)
+    for (let i = 0; i < givinPuans.length; i++) {
+      for (let j = 0; j < getingCountry.length; j++) {
+        if (givinPuans[i].previousElementSibling.previousElementSibling.textContent === getingCountry[j]) {
+          if (givenPuan[j] == 12) {
+            givinPuans[i].textContent = givenPuan[j];
+            totalPuans[i].textContent = (+totalPuans[i].textContent) + givenPuan[j];
+            document.getElementsByClassName('before-element')[i].style.width = "calc(100% - 140px)";
+            document.getElementsByClassName('before-element')[i].classList.add('points-12');
+          }
+        }
+      }
+      if (givinPuans[i].textContent != '0') {
+        givinPuans[i].style.display = 'inline-block';
+      }
+    }
+  }
+
+  let mainBoxArray = [];
+  for (let i = 0; i < mainBox.length; i++) {
+    mainBoxArray.push(mainBox[i]);
+  }
+
+  const moveBox = () => {
+    if (moveCount < 0) {
+      let t = 0;
+      scoreBoard.forEach((e) => {
+        if (e.result) {
+          t++;
+        }
+      })
+      console.log(t);
+      setTeleCountConst(t + 1);
+      setTeleCount(t + 1);
+      setBcgVideoLoop('src/videos/bcg-loop.mp4');
+    }
+
+    moveFunc()
+
+    moveElement();
+
+    if (moveCount < ((juri.length / 10) * 2)) {
+
+      if (moveCount % 2 !== 0 && moveCount >= 0) {
+        setTimeout(() => {
+          for (let i = 0; i < givinPuans.length; i++) {
+            givinPuans[i].style.display = 'none';
+            givinPuans[i].textContent = '0';
+            document.getElementsByClassName('before-element')[i].style.width = "0";
+            document.getElementsByClassName('before-element')[i].classList.remove('points-12');
+            setGivenPuan([]);
+            setGetingCountry([]);
+            setJuriCount(juriCount + 10);
+          }
+        }, 3000)
+      }
+    }
+
+    if (moveCount >= ((juri.length / 10) * 2)) {
+      for (let i = 0; i < givinPuans.length; i++) {
+        if (givinPuans[i].textContent != 0 && givinPuans[i].parentElement.style.backgroundColor !== 'rgb(30, 3, 62)') {
+          document.getElementsByClassName('before-element')[i].style.width = "calc(100% - 140px)";
+          setTimeout(() => {
+            givinPuans[i].parentElement.style.backgroundColor = 'rgb(30, 3, 62)';
+            givinPuans[i].previousElementSibling.style.backgroundImage = 'linear-gradient(to right, rgb(30, 3, 62), rgb(30, 3, 62))'
+            document.getElementsByClassName('before-element')[i].style.width = "0px";
+            givinPuans[i].style.display = 'none';
+          }, 2000)
+        }
+      }
+    }
+    setMoveCount(moveCount + 1);
+  }
+
+  useEffect(() => {
+    callData();
+  }, [])
+
+  let countryPuans = 0, arrayOfCountryPuans = [], arrayOfCountry = [];
+
+  allCountries.forEach((f) => {
+    if (f.result) {
+      finalists.forEach((e) => {
+        if (f.countryName === e.getingCountry) {
+          countryPuans += e.puan
+        }
+      })
+      arrayOfCountryPuans.push(countryPuans);
+      arrayOfCountry.push(f.countryName);
+      countryPuans = 0;
+    }
+  });
+
+
+
+  const tele = () => {
+    console.log(teleCount)
+    if (teleCount === teleCountConst) {
+
+      moveFunc()
+
+      mainBoxArray.reverse();
+      setMainBoxArrayTele(mainBoxArray);
+      document.getElementsByClassName('juri-tele')[0].style.display = 'none';
+      document.getElementsByClassName('main')[0].style.width = '65%';
+    }
+    else {
+      for (let i = 0; i < mainBoxArrayTele.length; i++) {
+        if (mainBoxArrayTele[teleCount - 1].children[2].textContent === arrayOfCountry[i]) {
+          mainBoxArrayTele[teleCount - 1].children[4].style.display = 'inline-block';
+          mainBoxArrayTele[teleCount - 1].children[4].textContent = arrayOfCountryPuans[i];
+          mainBoxArrayTele[teleCount - 1].children[3].textContent = (+mainBoxArrayTele[teleCount - 1].children[3].textContent) + arrayOfCountryPuans[i]
+        }
+      }
+    }
+    setTeleCount(teleCount - 1);
+  }
+
+
+
+  /// repeating function
+
+
+
+  const moveFunc = () => {
+    let arrayOfJuriTotal = [];
+
+    for (let i = 0; i < totalPuans.length; i++) {
+      arrayOfJuriTotal.push(+totalPuans[i].textContent)
+    }
+
+    let amount = 0, mainIndex = 0, n = arrayOfJuriTotal.length;
+
+    for (let j = 0; j < arrayOfJuriTotal.length; j++) {
+      for (let i = 0; i < n; i++) {
+        if (arrayOfJuriTotal[i] > amount) {
+          mainIndex = i;
+          amount = arrayOfJuriTotal[i];
+        }
+      }
+
+      let changeElement1 = arrayOfJuriTotal[n - 1]
+      arrayOfJuriTotal[n - 1] = arrayOfJuriTotal[mainIndex]
+      arrayOfJuriTotal[mainIndex] = changeElement1;
+
+      let changeElement2 = mainBoxArray[n - 1]
+      mainBoxArray[n - 1] = mainBoxArray[mainIndex]
+      mainBoxArray[mainIndex] = changeElement2;
+
+      n--;
+
+      mainIndex = 0
+      amount = 0
+    };
+  }
+
+
+  const moveElement = () => {
+    console.log(teleCountConst)
+    mainBoxArray.reverse()
+    let topCount1 = 0, topCount2 = 0;
+    for (let i = 0; i < mainBoxArray.length; i++) {
+      if (i < Math.ceil(teleCountConst / 2) - 1) {
+        mainBoxArray[i].style.top = topCount1 + 'px';
+        mainBoxArray[i].style.left = 0 + '%';
+        topCount1 += 50;
+      }
+      else {
+        mainBoxArray[i].style.top = topCount2 + 'px';
+        mainBoxArray[i].style.left = 51 + '%';
+        topCount2 += 50;
+      }
+    }
+
+    console.log(mainBoxArray, mainBoxArray[0])
+  }
+
+  return (
+    <>
+      {
+        window.innerWidth < 900 ? <div>This program is only sutible for computers</div> : <>
+          <div className={`back ${classOfBack}`}>
+            <video src={`${bcgVideo}`} style={{ width: '100%', height: '100%', objectFit: 'cover' }} autoPlay loop muted></video>
+          </div>
+          <section className='main'>
+            <div className='scoreboard'>
+              {
+                scoreBoard && scoreBoard.map((e) => {
+                  if (e.result) {
+                    return <div key={e.id} className='main-box'>
+                      <div className='before-element'></div>
+                      <img src={e.flag} alt="" />
+                      <span className='country-name'>{e.countryName}</span>
+                      <span className='total'>{0}</span>
+                      <span className='givin'>{0}</span>
+                    </div>
+                  }
+                })
+              }
+            </div>
+            <div className='juri-tele'>
+              <video src='./videos/and.mp4' autoplay=""></video>
+              <div>
+                <p>
+                  <span>{countOfJuri}</span>
+                  <span> of {(juri.length / 10)} Countries</span>
+                </p>
+                <span className='name-of-juri'>{nameOfJuri}</span>
+              </div>
+            </div>
+          </section>
+
+          <button onClick={showVotes} className='click click1'>Show</button>
+          <button onClick={addJuri} className='click click2'>AddJuri</button>
+          <button onClick={add12} className='click click3'>Add12</button>
+          <button onClick={moveBox} className='click click4'>Move</button>
+          <button onClick={tele} className='click click5'>Tele</button></>
+      }
+    </>
+  )
+}
+
+export default App
